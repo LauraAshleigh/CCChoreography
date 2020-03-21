5bde4d320d382895fd40dea8b6f1f228e221dda4
Laura Cronin <cronin.laura91@gmail.com>
2020-03-21 18:34:43 +1100

    Change testimonials page

--- src/Components/TestimonialsPage/TestimonialsPage.js
+++ src/Components/TestimonialsPage/TestimonialsPage.js
@@ -1,20 +1,50 @@
 import React from 'react'
 import PageWrapper from '../PageWrapper/PageWrapper'
 import testimonials from './TestimonialsConfig'
+import ccsymbol from '../../images/symbol-grey.png'
 import './TestimonialsPage.css'
 
 const testimonialsPage = () => {
   return (
     <PageWrapper title='Testimonials'>
-      <div className='testimonials-grid'>
-        {testimonials.map(testimonial =>
-          <blockquote className='testimonial' key={testimonial.name}>
-            {testimonial.review}
-            <cite> {testimonial.name} - {testimonial.year}</cite>
+      {testimonials.map((testimonial, key) =>
+        <React.Fragment key={testimonial.name}>
+          <div className='testimonial-grid' >
 
-          </blockquote>
-        )}
-      </div>
+          <div className='stars-and-name grow'>
+            <img src={ccsymbol} alt='CC Choreography' />
+            <div>
+              <div className='stars'>
+                {[...Array(testimonial.stars)].map(star =>
+                  <i key={star} className='fas fa-star' />
+                )}
+              </div>
+              <div> {testimonial.name} ({testimonial.year})</div>
+            </div>
+          </div>
+            
+            {/* <div>
+              <div>
+                {[...Array(testimonial.stars)].map(star =>
+                  <i key={star} className='fas fa-star' />
+                )}
+              </div>
+              <div> {testimonial.name} ({testimonial.year})</div>
+            </div> */}
+            
+            <div className='review'>
+            
+              <div className='quotation-mark'>&#8220;</div>
+              <div className='review-text'>{testimonial.review}</div>
+              
+            </div>
+
+          </div>
+          {key === testimonials.length - 1 ? null :
+            <hr/>
+          }
+        </React.Fragment>
+      )}
 
 
     </PageWrapper>
